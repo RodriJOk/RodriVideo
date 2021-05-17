3938e3b4ff3e56a89a301a93f8e34f6f7f16b4fb
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-05-17 14:52:17 -0300

    Add Recaptcha in /register, modifications in styles

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import { connect } from 'react-redux'
 import { registerRequest } from '../actions/index'
 import { Link } from 'react-router-dom'
 import '../assets/styles/components/Register.css'
+import ReCAPTCHA from 'react-google-recaptcha'
 
 const Register = (props) => {
 
@@ -11,6 +12,14 @@ const [form, setValues] = useState({
     email: "",
     password: ""
 })
+
+const captcha = useRef(null);
+
+const elrecaptcha = () =>{
+    if(captcha.current.getValue()){
+        console.log("El usuario dio Ok en el captcha")
+    }
+}
 
 const handleInput = event =>{
     setValues({
@@ -49,12 +58,21 @@ const handleSubmit = event =>{
                  className="input" 
                  placeholder="Contraseña"
                  onChange={handleInput}/>
-                
+
+                <ReCAPTCHA
+                    ref= {captcha}
+                    sitekey= "6LdMGdkaAAAAAOI6vEmWDxxB0Tp5Vxc_2r8dtq1v"
+                    onChange= {elrecaptcha}
+                />
+
                 <button className="button">Registrarme</button>
             </form>
-            <Link to="/login">
-                Iniciar Sesion
-            </Link>
+            <div className="login__container--section">
+                <p>¿Ya tienes cuenta ?</p>
+                    <Link to="/login">
+                       <span>Iniciar Sesion</span> 
+                    </Link>
+            </div>
         </section>
     </section>
     )
